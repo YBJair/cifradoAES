395f41eec86cf83dfaf45e371e817737346fbc65
Jair Abel Jiménez Bellocchio <jajb1994@hotmail.com>
2016-12-07 14:42:09 +0100

    Ahora es un proyecto de java que podeis usar desde Eclipse

---
+++ src/interfaz.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class interfaz extends Frame implements ActionListener{
+
+    public interfaz(){
+    	
+    }
+
+    //Poned aqui vuestros metodos.
+
+    //
+    public static void main(String[] args){
+        new interfaz();
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
