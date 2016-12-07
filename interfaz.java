b2087a0a40b83c568a614a083c400bfef2c71a14
Jair Abel Jiménez Bellocchio <jajb1994@hotmail.com>
2016-12-07 12:40:01 +0100

    La base

---
+++ interfaz.java
@@ -0,0 +1,16 @@
+import java.awt.*
+
+public class Interfaz extends Frame{
+
+    public Interfaz(){
+
+    }
+
+    //Poned aquí vuestros métodos.
+
+    //
+    public static void main(String[] args){
+        new Interfaz();
+    }
+
+}
